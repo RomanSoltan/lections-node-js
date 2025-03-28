04eed051b099a9fbe9b36668f2bf23ffb3790ab2
Roman Soltan <romabandit88@gmail.com>
2025-03-28 20:16:23 +0200

    pino, cors

--- module_2/lection_3/express-example-3/src/index.js
+++ module_2/lection_3/express-example-3/src/index.js
@@ -1,7 +1,35 @@
 import express from "express";
+import cors from "cors";
+import PinoHttp from "pino-http";
+
 import movies from "./db/movies.js";
 
 const app = express();
+
+app.use(cors());
+
+// const logger = PinoHttp({
+//   transport: {
+//     target: "pino-pretty",
+//   },
+// });
+
+// app.use(logger);
+
+// // цей middleware спрацьовує для будь-якого запиту і дозволяє крос-доменні запити, змінюючи заголовок. Але так уже не пишуть
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
 // add middleware
 // перший аргумент або адреса або функція
@@ -37,7 +65,7 @@ app.listen(3000, () => console.log("server running on 3000 port"));
 // middleware - це проміжий обробник, коли певна задача вимагає кілька етапів обробки. Бувають глобальні і локальні middleware
 
 // Основий принцип express обробки middleware:
-// - коли приходить запит певного методу на певну адресу express
+// - коли приходить запит певного методу на певну адресу, express
 // послідовно перевіряє всі записи в тому порядку, в якому вони
 // написані у коді, коли знаходить перший потрібний middleware,
 // то виконує функцію, яка прописана у ньому і далі express
