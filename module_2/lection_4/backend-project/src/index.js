4746871d5bb7c908955d1aa87549d163fbc5e974
Roman Soltan <romabandit88@gmail.com>
2025-03-29 19:42:15 +0200

    set connection to database

--- module_2/lection_4/backend-project/src/index.js
+++ module_2/lection_4/backend-project/src/index.js
@@ -1,3 +1,9 @@
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { startServer } from './server.js';
 
-startServer();
+const bootstrap = async () => {
+  // функцію підключення до бази потрібно викликати до запуску сервера
+  await initMongoConnection();
+  startServer();
+};
+bootstrap();
