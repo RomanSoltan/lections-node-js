c0d84cfa2c6eff560a311f3aae5f810daba223ef
Roman Soltan <romabandit88@gmail.com>
2025-04-02 19:18:20 +0300

    middleware err

--- module_3/lection_5/project-backend/src/controllers/movies.js
+++ module_3/lection_5/project-backend/src/controllers/movies.js
@@ -1,31 +1,43 @@
+import createHttpError from 'http-errors';
 import { getMovieById, getMovies } from '../services/movies.js';
 
-export const getMoviesController = async (req, res) => {
-  // робимо запит до колекції
-  const data = await getMovies();
+export const getMoviesController = async (req, res, next) => {
+  try {
+    // робимо запит до колекції
+    const data = await getMovies();
 
-  res.json({
-    status: 200,
-    message: 'Successfully find movies!',
-    data,
-  });
-};
-
-export const getMovieByIdController = async (req, res) => {
-  // витягнемо id з req, де він зберігається
-  const { id } = req.params;
-  const data = await getMovieById(id);
-
-  if (!data) {
-    return res.status(404).json({
-      status: 404,
-      message: `Movie with id=${id} not found`,
+    res.json({
+      status: 200,
+      message: 'Successfully find movies!',
+      data,
     });
+  } catch (error) {
+    next(error);
   }
-
-  res.json({
-    status: 200,
-    message: `Successfully find movie with id=${id}!`,
-    data,
-  });
 };
+
+export const getMovieByIdController = async (req, res, next) => {
+  try {
+    // витягнемо id з req, де він зберігається
+    const { id } = req.params;
+    const data = await getMovieById(id);
+
+    if (!data) {
+      throw createHttpError(404, `Movie with id=${id} not found`);
+    }
+
+    res.json({
+      status: 200,
+      message: `Successfully find movie with id=${id}!`,
+      data,
+    });
+  } catch (error) {
+    // передається в обробник помилок з 4-ма параметрами errorHandler
+    next(error);
+  }
+};
+
+// Замість того, щоб в кожному catch передавати відповідь з помилкою,
+// можна помилку передати в next() і тоді express буде шукати обробник
+// помилок, обробником помилок express вважає middleware, і якому є
+// 4 параметри, де викликає функцію, куди першим аргументом передає помилку
--- module_3/lection_5/project-backend/src/middlewares/errorHandler.js
+++ module_3/lection_5/project-backend/src/middlewares/errorHandler.js
@@ -1,5 +1,6 @@
 export const errorHandler = (err, req, res, next) => {
-  res.status(500).json({
-    message: err.message,
+  const { status = 500, message = 'Server error' } = err;
+  res.status(status).json({
+    message,
   });
 };
