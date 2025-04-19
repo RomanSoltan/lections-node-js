25286f8fce041ec8c24cb973e7f0a61942bb8b00
Roman Soltan <romabandit88@gmail.com>
2025-04-19 13:33:04 +0300

    hashPassword

--- module_5/lection_9.Authentication/project-backend/src/services/auth.js
+++ module_5/lection_9.Authentication/project-backend/src/services/auth.js
@@ -1,15 +1,25 @@
+import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import UserCollection from '../db/models/User.js';
 
 export const registerUser = async (payload) => {
-  const { email } = payload;
+  const { email, password } = payload;
   // Шукаємо користувача в базі за email, якщо є, то
   // викидаємо помилку зі статусом і текстом помилки
-  const user = await UserCollection.find({ email }); // поверне null або object
+  const user = await UserCollection.findOne({ email }); // поверне null або object
+  // console.log(Boolean([])); // true
+
   if (user) {
     throw createHttpError(409, 'Email already in use');
   }
 
-  // Створюємо нового користувача і зберігаємо у базі
-  return await UserCollection.create(payload);
+  // хешуємо пароль, який передає користувач
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  // Створюємо нового користувача і зберігаємо його
+  // у базі разом із захешованим паролем
+  return await UserCollection.create({
+    ...payload,
+    password: hashPassword,
+  });
 };
