2c4737ceb3441666b76a07c1af7418e8ebc24f03
Roman Soltan <romabandit88@gmail.com>
2025-04-25 06:39:05 +0300

    nodemailer

--- module_6/lection-11.SMTP_and_mail/nodemailer-example/app.js
+++ module_6/lection-11.SMTP_and_mail/nodemailer-example/app.js
@@ -1,28 +1,32 @@
 import nodemailer from "nodemailer";
 import "dotenv/config";
 
-const {UKR_NET_EMAIL, UKR_NET_PASSWORD} = process.env;
+const { UKR_NET_EMAIL, UKR_NET_PASSWORD } = process.env;
 
 const nodemailerConfig = {
-    host: "smtp.ukr.net",
-    port: 465, // 25, 465, 2525
-    secure: true,
-    auth: {
-        user: UKR_NET_EMAIL,
-        pass: UKR_NET_PASSWORD,
-    }
+  // адреса поштового серверу, до якого підключаємось
+  host: "smtp.ukr.net",
+  port: 465,
+  secure: true,
+  //   email до якого підключаємось
+  auth: {
+    user: UKR_NET_EMAIL,
+    pass: UKR_NET_PASSWORD,
+  },
 };
 
+// обєкт, який відпрвляє email
 const transport = nodemailer.createTransport(nodemailerConfig);
 
 const email = {
-    from: UKR_NET_EMAIL,
-    to: "goxosam266@agiuse.com",
-    subject: "Hello",
-    text: "Hello",
-    html: "<h1>Hello!</h1>"
+  from: UKR_NET_EMAIL,
+  to: "sixako1841@agiuse.com",
+  subject: "Hello",
+  text: "Hello",
+  html: "<h1>Hello my friend!</h1>",
 };
 
-transport.sendMail(email)
-    .then(msg => console.log(msg))
-    .catch(error => console.log(error));
+transport
+  .sendMail(email)
+  .then((msg) => console.log(msg))
+  .catch((error) => console.log(error));
--- module_6/lection-11.SMTP_and_mail/nodemailer-example/utils/sendEmail.js
+++ module_6/lection-11.SMTP_and_mail/nodemailer-example/utils/sendEmail.js
@@ -1,30 +1,31 @@
 import nodemailer from "nodemailer";
 import "dotenv/config";
 
-const {UKR_NET_EMAIL, UKR_NET_PASSWORD} = process.env;
+const { UKR_NET_EMAIL, UKR_NET_PASSWORD } = process.env;
 
 const nodemailerConfig = {
-    host: "smtp.ukr.net",
-    port: 465, // 25, 465, 2525
-    secure: true,
-    auth: {
-        user: UKR_NET_EMAIL,
-        pass: UKR_NET_PASSWORD,
-    }
+  // адреса поштового серверу, до якого підключаємось
+  host: "smtp.ukr.net",
+  port: 465,
+  secure: true,
+  //   email до якого підключаємось
+  auth: {
+    user: UKR_NET_EMAIL,
+    pass: UKR_NET_PASSWORD,
+  },
 };
 
+// обєкт, який відпрвляє email
 const transport = nodemailer.createTransport(nodemailerConfig);
 
-/*
-const data = {
-    to: "goxosam266@agiuse.com",
-    subject: "Hello",
-    text: "Hello",
-    html: "<h1>Hello!</h1>"
-};
-*/
+// const data = {
+//   to: "sixako1841@agiuse.com",
+//   subject: "Hello",
+//   text: "Hello",
+//   html: "<h1>Hello my friend!</h1>",
+// };
 
-export const sendEmail = data => {
-    const email = {...data, from: UKR_NET_EMAIL};
-    return transport.sendMail(email);
-}
+export const sendEmail = (data) => {
+  const email = { ...data, from: UKR_NET_EMAIL };
+  return transport.sendMail(email);
+};
