57e32671b70bea61815555118bb52b76f17a166a
Roman Soltan <romabandit88@gmail.com>
2025-05-05 06:01:27 +0300

    getAllMovies

--- module_7/lection_14.Swagger/project-backend/src/middlewares/errorHandler.js
+++ module_7/lection_14.Swagger/project-backend/src/middlewares/errorHandler.js
@@ -1,8 +1,9 @@
 export const errorHandler = (err, req, res, next) => {
   const { status = 500, message = 'Server error' } = err;
-  console.log('errorHandler---', err);
+  // console.log('errorHandler---', err);
 
   res.status(status).json({
+    status,
     message,
   });
 };
